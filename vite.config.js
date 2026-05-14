1146a4563f785bae881932dc72b3ad57ad8a3a24
Aman Medatwal <chhipaaman365@gmail.com>
2026-05-14 22:36:58 +0530

    fix vercel paths

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Maryada-Films/',
-  
+  base: '/',
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
